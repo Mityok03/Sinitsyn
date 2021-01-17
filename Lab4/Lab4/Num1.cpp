1ae5dbbcadef2932b031d4d9c5b96ae12552b0c4
Sind <mityo4ek@gmail.com>
2021-01-17 19:31:07 +0300

    Num1

---
+++ Lab4/Lab4/Num1.cpp
@@ -0,0 +1,12 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    double a, b, c, d;
+    cin >> a >> b;
+    c = a * b;
+    d = 2 * (a + b);
+    cout << endl << c << endl << d;
+}
